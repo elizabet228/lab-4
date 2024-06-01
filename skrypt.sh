30ca2fcfa977d5853f148156036e20e054065b90
Elizabet <liza.golemba@gmail.com>
2024-06-01 20:46:54 +0200

    Add short flags -d, -l, -h

--- skrypt.sh
+++ skrypt.sh
@@ -8,3 +8,6 @@ esac
   -h) echo "Available options: --date, --logs [number], --help" ;;
   -d) date ;;
   -l) num=${2:-30}; for i in $(seq 1 $num); do echo "log$i.txt - skrypt.sh - $(date)" > log$i.txt; done ;;
+  -h) echo "Available options: --date, -d, --logs [number], -l [number], --help, -h" ;;
+  -l) num=${2:-30}; for i in $(seq 1 $num); do echo "log$i.txt - skrypt.sh - $(date)" > log$i.txt; done ;;
+  -d) date ;;
