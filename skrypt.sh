90035c11bc36c320d9fb4fe9638ca3b1194ea4fc
Elizabet <liza.golemba@gmail.com>
2024-06-01 21:01:03 +0200

    Finalize help option

--- skrypt.sh
+++ skrypt.sh
@@ -5,10 +5,19 @@ case $1 in
 esac
 --logs) for i in {1..100}; do echo "log$i.txt - skrypt.sh - $(date)" > log$i.txt; done ;;
   --help) echo "Available options: --date, --logs [number], --help" ;;
+  -h) echo "Available options: --date, -d, --logs 100, -l 100, --help, -h, --init, --error 100, -e 100" ;;
+  -h) echo "Available options: --date, -d, --logs [number], -l [number], --help, -h, --init, --error [number], -e [number]" ;;
+  -h) echo "Available options: --date, -d, --logs 100, -l 100, --help, -h, --init, --error 100, -e 100" ;;
   -h) echo "Available options: --date, --logs [number], --help" ;;
+  -h) echo "Available options: --date, -d, --logs 100, -l 100, --help, -h, --init, --error 100, -e 100" ;;
+  -h) echo "Available options: --date, -d, --logs [number], -l [number], --help, -h, --init, --error [number], -e [number]" ;;
+  -h) echo "Available options: --date, -d, --logs 100, -l 100, --help, -h, --init, --error 100, -e 100" ;;
   -d) date ;;
   -l) num=${2:-30}; for i in $(seq 1 $num); do echo "log$i.txt - skrypt.sh - $(date)" > log$i.txt; done ;;
   -h) echo "Available options: --date, -d, --logs [number], -l [number], --help, -h" ;;
+  -h) echo "Available options: --date, -d, --logs 100, -l 100, --help, -h, --init, --error 100, -e 100" ;;
+  -h) echo "Available options: --date, -d, --logs [number], -l [number], --help, -h, --init, --error [number], -e [number]" ;;
+  -h) echo "Available options: --date, -d, --logs 100, -l 100, --help, -h, --init, --error 100, -e 100" ;;
   -l) num=${2:-30}; for i in $(seq 1 $num); do echo "log$i.txt - skrypt.sh - $(date)" > log$i.txt; done ;;
   -d) date ;;
   --init) git clone https://github.com/elizabet228/lab-4.git && export PATH=$PATH:$(pwd)/lab-4 ;;
