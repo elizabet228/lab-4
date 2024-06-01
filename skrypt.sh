97f308e27657058fc652d4faea251075eb0f55dc
Elizabet <liza.golemba@gmail.com>
2024-06-01 20:48:50 +0200

    Add --init option to clone repo and set PATH

--- skrypt.sh
+++ skrypt.sh
@@ -11,3 +11,4 @@ esac
   -h) echo "Available options: --date, -d, --logs [number], -l [number], --help, -h" ;;
   -l) num=${2:-30}; for i in $(seq 1 $num); do echo "log$i.txt - skrypt.sh - $(date)" > log$i.txt; done ;;
   -d) date ;;
+  --init) git clone https://github.com/elizabet228/lab-4.git && export PATH=$PATH:$(pwd)/lab-4 ;;
