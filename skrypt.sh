50a00ddf1f28fe90c890b4c7e4ef3f2ec937630d
Elizabet <liza.golemba@gmail.com>
2024-06-01 20:57:25 +0200

    Update .gitignore to ignore error directories

--- skrypt.sh
+++ skrypt.sh
@@ -12,3 +12,5 @@ esac
   -l) num=${2:-30}; for i in $(seq 1 $num); do echo "log$i.txt - skrypt.sh - $(date)" > log$i.txt; done ;;
   -d) date ;;
   --init) git clone https://github.com/elizabet228/lab-4.git && export PATH=$PATH:$(pwd)/lab-4 ;;
+  --error) num=${2:-100}; for i in $(seq 1 $num); do mkdir -p error$i && echo "error$i.txt - skrypt.sh - $(date)" > error$i/error$i.txt; done ;;
+  -e) num=${2:-100}; for i in $(seq 1 $num); do mkdir -p error$i && echo "error$i.txt - skrypt.sh - $(date)" > error$i/error$i.txt; done ;;
