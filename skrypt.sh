d42a59f58294ac8e2b4cf07bce208afbfa35ae2b
Elizabet <liza.golemba@gmail.com>
2024-06-01 20:43:01 +0200

    Add --help option to display available options

--- skrypt.sh
+++ skrypt.sh
@@ -4,3 +4,7 @@ case $1 in
 *) echo "Invalid option" ;;
 esac
 --logs) for i in {1..100}; do echo "log$i.txt - skrypt.sh - $(date)" > log$i.txt; done ;;
+  --help) echo "Available options: --date, --logs [number], --help" ;;
+  -h) echo "Available options: --date, --logs [number], --help" ;;
+  -d) date ;;
+  -l) num=${2:-30}; for i in $(seq 1 $num); do echo "log$i.txt - skrypt.sh - $(date)" > log$i.txt; done ;;
